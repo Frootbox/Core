9c059769fc043f8517833eff375626b8a9f0f703
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2025-07-14 07:59:21 +0200

    general update

--- cms/extensions/Core/HelpAndSupport/classes/Plugins/Testimonials/Persistence/Repositories/Testimonials.php
+++ cms/extensions/Core/HelpAndSupport/classes/Plugins/Testimonials/Persistence/Repositories/Testimonials.php
@@ -10,5 +10,7 @@ namespace Frootbox\Ext\Core\HelpAndSupport\Plugins\Testimonials\Persistence\Repo
  */
 class Testimonials extends \Frootbox\Persistence\Repositories\AbstractAssets
 {
+    use \Frootbox\Persistence\Repositories\Traits\Tags;
+
     protected $class = \Frootbox\Ext\Core\HelpAndSupport\Plugins\Testimonials\Persistence\Testimonial::class;
 }
